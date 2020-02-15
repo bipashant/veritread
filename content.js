f25e2aa446ffc51ffcca363f2df3e47c879078db
Bibek Sharma Chapagain <bs_chapagain@hotmail.com>
2020-02-15 11:06:59 +1100

    Create content.js

---
+++ content.js
@@ -0,0 +1,5 @@
+var serial_label = "Serial # (if applicable, or enter NA) [This info is not publicly viewable]";
+
+var value_of_equipment_label = "Value of equipment [This info is not publicly viewable]";
+
+var noListingFoundText = "Didn't find what you need? Tell us and we'll get it for you. Click the blue chat bubble below to send a message to a Burly team member now.";
